4b1a68fdda0c451506904f4b9e6cdee42f6bfaee
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2021-05-22 20:56:50 -0400

    Create tablePrinter.py
    Simple Start

---
+++ tablePrinter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 20:39:21 2021
+
+@author: williammcintyre
+"""
+
+# Table Printer
+# Practice Project
+
+tableData = [['apples', 'oranges', 'cherries', 'bananas'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def findWidth():
+    colWidth = []
+    for column in tableData:
+        longestString = ''
+        for value in column:
+            if len(value) > len(longestString):
+                longestString = value
+        colWidth.append(longestString)
+    return colWidth
